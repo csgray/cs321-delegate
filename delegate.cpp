27bff6ed726fa0d4048f023a019dbbb2c0f46c51
Corey Gray <csgray@alaska.edu>
2018-02-12 22:26:40 -0900

    Add user input.

--- delegate.cpp
+++ delegate.cpp
@@ -3,3 +3,43 @@
 // 02 February 2018
 // Source code for Assignment 2: Delegated Computation program
 
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+#include <limits>
+#include <mutex>
+using std::mutex;
+#include <thread>
+using std::thread;
+#include <vector>
+using std::vector;
+
+#include "sa2a.h"	// For "int sa2a(int)"
+
+int main() {
+	cout << "Delegated Computation" << endl;
+	
+	int input;
+	vector<int> inputBuffer;
+
+	while (true) {
+		cout << "Enter a positive integer (or 0 to end input): ";
+		cin >> input;
+
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+			cout << "Only integers are valid input." << endl;
+			continue;
+		}
+
+		else if (input < 1)
+			break;
+	
+		inputBuffer.push_back(input);
+	}
+
+	for (auto i : inputBuffer)
+		cout << i << endl;
+}
